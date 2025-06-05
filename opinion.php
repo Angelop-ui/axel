a50313106a00fb51809c526a17fb0c480388c3c9
Angelop-ui <angeljesusortizlopez@gmail.com>
2025-06-05 08:57:22 -0600

    Update opinion.php

--- opinion.php
+++ opinion.php
@@ -1,5 +1,5 @@
 <h2>Déjanos tu Opinión</h2>
-<form method="post" action="pages/guardar_opinion.php">
+<form method="post" action="guardar_opinion.php">
     <label for="comentario">Comentario:</label>
     <textarea name="comentario" id="comentario" rows="6" required placeholder="Escribe tu opinión aquí..."></textarea>
     <br>
